164589c1b5907de04f3dbe194768997c9eaba4ff
German Flores <gfloresa@ucsd.edu>
2018-02-14 15:44:08 -0800

    added segue to detailController from superhero tab

---
+++ Wk1_Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Wk1_Flix
+//
+//  Created by German Flores on 2/13/18.
+//  Copyright © 2018 German Flores. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
